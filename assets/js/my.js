30db920417d7085d041edf199f43d40d88013bbb
emranmd <emran01851384660@gmail.com>
2023-11-18 15:38:26 +0600

    init

---
+++ assets/js/my.js
@@ -0,0 +1,12 @@
+$('.foodsItems_imageAndText').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+  
+  $('.foodCarosule_containerWrp').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+  });
