7c717cb4015d2f036a002e174056a93f799bcc5b
emranmd <emran01851384660@gmail.com>
2023-11-21 16:28:16 +0600

    food items

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,12 +1,6 @@
-$('.foodsItems_imageAndText').slick({
+$('.foodsItems_imageAndName').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
-  });
-  
-  $('.foodCarosule_containerWrp').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
   });
